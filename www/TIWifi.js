9454aeba6b1e49175836b3411ed8decf89c3c01d
vidding <skyyu164@gmail.com>
2015-11-18 18:04:10 +0800

    add smart config function

--- www/TIWifi.js
+++ www/TIWifi.js
@@ -9,13 +9,31 @@ TIWifi.prototype.getssid = function(successCallback, errorCallback){
     cordova.exec(successCallback, errorCallback, "TIWifi", "getssid", []);
 };
 
-TIWifi.prototype.setssid = function(ssid, password, successCallback, errorCallback){
+/*
+ * 开始配置SSID...
+ * ssid不能为空，其它三个参数都可以为""，不能为null
+ *
+ */
+TIWifi.prototype.startconfig = function(ssid, password, devname, encodekey, successCallback, errorCallback){
     var options = {
         ssid: ssid,
-        password: password
+        password: password,
+        devname: devname,
+        encodekey: encodekey
     };
-    cordova.exec(successCallback, errorCallback, "TIWifi", "setssid", [options]);
+    cordova.exec(successCallback, errorCallback, "TIWifi", "startconfig", [options]);
 };
+
+TIWifi.prototype.stopconfig = function(successCallback, errorCallback){
+    cordova.exec(successCallback, errorCallback, "TIWifi", "stopconfig", []);
+};
+TIWifi.prototype.startfinddevice = function(successCallback, errorCallback){
+    cordova.exec(successCallback, errorCallback, "TIWifi", "startfinddevice", []);
+};
+TIWifi.prototype.stopfinddevice = function(successCallback, errorCallback){
+    cordova.exec(successCallback, errorCallback, "TIWifi", "stopfinddevice", []);
+};
+
 
 TIWifi.install = function () {
     if (!window.plugins) {
